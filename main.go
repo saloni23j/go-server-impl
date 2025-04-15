f2e6941faa48de826028329398b74ba1399ef52a
saloni23j <mathursaloni123@gmail.com>
2025-04-15 17:41:50 +0530

    mux server with CRUD done

--- main.go
+++ main.go
@@ -4,22 +4,28 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"sync"
 )
 
 type User struct {
 	Name string `json:"name"`
-	// Age  string `json:"age"`
+	Age  string `json:"age"`
 }
 
 var userCache = make(map[int]User)
+var cacheMutex sync.RWMutex
 
 func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handleRoot)
-	fmt.Println("LEESSGOOOOOOOOOOOOOO")
-	http.ListenAndServe(":8080", mux)
+
 	mux.HandleFunc("POST /users", createUser)
+	mux.HandleFunc("GET /users/{id}", getUser)
+	mux.HandleFunc("DELETE /users/{id}", deleteUser)
+	fmt.Println("SERVER RUNNING ON :8080")
+	http.ListenAndServe(":8080", mux)
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
@@ -34,9 +40,62 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	if user.Name == "" {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, "name is reqd", http.StatusBadRequest)
 		return
 	}
+	cacheMutex.Lock()
 	userCache[len(userCache)+1] = user
+	cacheMutex.Unlock()
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	idStr := r.PathValue("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	cacheMutex.RLock()
+	user, ok := userCache[id]
+	cacheMutex.RUnlock()
+	if !ok {
+		http.Error(
+			w, "user not found", http.StatusNotFound,
+		)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	// j, err := json.Marshal(user)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+	// w.WriteHeader(http.StatusOK)
+	// w.Write(j)
+	json.NewEncoder(w).Encode(user)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	idStr := r.PathValue("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(
+			w,
+			err.Error(),
+			http.StatusBadRequest,
+		)
+		return
+	}
+	if _, ok := userCache[id]; !ok {
+		http.Error(
+			w,
+			"user not found",
+			http.StatusBadRequest,
+		)
+		return
+	}
+	cacheMutex.Lock()
+	delete(userCache, id)
+	cacheMutex.Unlock()
 	w.WriteHeader(http.StatusNoContent)
 }
